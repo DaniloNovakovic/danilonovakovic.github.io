8672d68ec2e9536f3cf91b9f991f255ed7548f94
DaniloNovakovic <dakenzi97@gmail.com>
2018-07-20 15:37:10 +0200

    refactored code
    - Organized code and renamed div's to coresponding header,footer,main,section  appropriately to make code mode readable / easier to navigate.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // Mobile nav functionality
-    $('#hamburger,#close').click(function() {
-        $('#hamburger, #close').toggle();
+    $('#open-nav,#close').click(function() {
+        $('#open-nav, #close').toggle();
         $('.custom-navigation #links').toggleClass('visible');
     });
 });
