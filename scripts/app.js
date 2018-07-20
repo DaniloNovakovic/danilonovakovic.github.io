cc7286907fcba92e21fdd9b554090ac2b9c9d0ed
DaniloNovakovic <dakenzi97@gmail.com>
2018-07-20 13:45:42 +0200

    Added mobile support aswell as additional links
    - Page now resizes acordingly due to the width of the screen and navbar is created. This makes it more readable for mobile users.

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Mobile nav functionality
+    $('#hamburger,#close').click(function() {
+        $('#hamburger, #close').toggle();
+        $('.custom-navigation #links').toggleClass('visible');
+    });
+});
