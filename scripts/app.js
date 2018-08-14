7b344514e21225b05bceb63f57946dbaeb68feda
Danilo Novakovic <dakenzi97@gmail.com>
2018-08-14 23:43:16 +0200

    last commit for version 1
    - complete version 1

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Mobile nav functionality
+    $('#open-nav,#close').click(function() {
+        $('#open-nav, #close').toggle();
+        $('.custom-navigation #links').toggleClass('visible');
+    });
+    $('.custom-navigation #links').click(function() {
+       $('.custom-navigation #links').removeClass('visible'); 
+        if($('#close').is(':visible')) {
+            $('#open-nav').show();
+            $('#close').hide();
+        }
+    });
+});
