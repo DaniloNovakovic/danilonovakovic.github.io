6366cc90ddda31a024f26305c7c193814aa23ebd
Danilo Novakovic <dakenzi97@gmail.com>
2018-08-28 22:12:56 +0200

    removed jquery (because i didn't use it for anything)

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,1 @@
 const ABOUT_ME = '#about-me';
-
-$(document).ready(function () {
-    $(window).on('resize', function () {
-        if ($(window).height() > $(window).width()) {
-            $(ABOUT_ME).css('grid-column', '1 / span 2');
-        } else {
-            $(ABOUT_ME).css('grid-column','');
-        }
-    });
-
-});
